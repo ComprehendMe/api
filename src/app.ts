8b7bf3c0497a78b917d908fbfbad6d9be7c1ff82
JoelTorcato <a12022@ae4outubro.pt>
2025-10-23 12:43:41 +0100

    Add Swagger API documentation with @elysiajs/swagger
    Introduced the @elysiajs/swagger dependency and integrated it into the app to provide automatic Swagger API documentation. This enhances API discoverability and developer experience.

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import { createApp } from "./entry";
 import { env } from "./common/env";
 import { routify } from "./common/routify";
 import chalk from "chalk";
+import swagger from '@elysiajs/swagger';
 
 export const app = await createApp();
 
 await routify(app);
+app.use(swagger());
 
 app.listen(env.PORT, async () => {
   app.decorator.readyAt = Date.now();
