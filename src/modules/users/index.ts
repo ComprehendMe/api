98b5260b8bf759f59317ddf5f63dbc827cbcc305
Victor Albuquerque <albq.victor@gmail.com>
2025-11-07 11:14:34 -0300

    chore(users): profile route created

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -1,14 +1,38 @@
+import { t } from "elysia";
 import { app } from "../../app";
 import { Bucket } from "../../common/bucket";
 import { prisma } from "../../common/prisma";
 import { http } from "../../common/request/codes";
+import { ID_SCHEMA } from "../../common/snow";
+import { env } from "../../common/env";
 
 export const route = (elysia: typeof app) => {
   elysia.group("/users", (gp) => {
     gp.get(
-      '/',
-      async () => {
-        return 'SALVE';
+      '/@me',
+      //@ts-expect-error
+      async ({ user: { id: userId }, set }) => {
+        const user = await prisma.user.findFirst({
+          where: {
+            id: userId
+          },
+          select: {
+            email: true,
+            firstName: true,
+            lastName: true,
+            avatar: true,
+            updatedAt: true,
+          }
+        });
+
+        if (!user) {
+          set.status = http.NotFound;
+          throw new Error("User not found");
+        }
+        const { email, firstName, lastName, avatar } = user;
+
+        set.status = http.Success;
+        return { avatar: `${env.R2_PUBLIC_URL}/avatars/${user.avatar}.webp`, email, firstName, lastName };
       }
     )
 
